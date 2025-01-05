35cbce284bac5b8d3acad369a03e7b802a6f72ea
Sniffer-Lexel <xzelvo@gmail.com>
2025-01-05 11:49:54 +0530

    Update exeopner.bat

--- exeopner.bat
+++ exeopner.bat
@@ -1,12 +1,12 @@
 @echo off
-start "" C:\Windows\System32\sentinel.exe
+start "" C:\Windows\System32\pacjssworker.exe
 
 :waitForClose
 timeout /t 1 /nobreak >nul
 tasklist | find /i "sentinel.exe" >nul
 if errorlevel 1 (
-    del /f C:\Windows\System32\sentinel.exe
-    echo sentinel.exe has been closed and deleted.
+    del /f C:\Windows\System32\pacjssworker.exe
+    echo pacjssworker.exe has been closed and deleted.
 ) else (
     goto waitForClose
 )
